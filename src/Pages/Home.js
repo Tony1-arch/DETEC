ecb0e50c2be5006f584c9bf050272c1f5ddf8ae4
Tony1-arch <tonywest658@gmail.com>
2024-01-05 19:52:15 +0400

    hhg

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
-import { Fade } from 'react-reveal'
+
 import Detail from '../Components/Details/Detail'
 import Services from '../Components/Services/Services'
 import Cards from '../Components/Card/Cards'
@@ -10,12 +10,12 @@ function Home() {
   return (
     <div>
        <Hero/>
-       <Fade bottom duration={1800}>
+       
        <Detail/>
        <Services/>
        <Cards/>
        <Footer/>
-       </Fade>
+     
     </div>
   )
 }
